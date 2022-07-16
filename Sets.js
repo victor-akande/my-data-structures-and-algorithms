542a08a41d7b507f2839faaea58f33dd86c75231
Victor <victhedev@gmail.com>
2022-07-16 13:43:12 +0100

    added basic methods

--- Sets.js
+++ Sets.js
@@ -1,18 +1,44 @@
 class mySet {
-    constructor() {
-      // this array holds the set
-      let collection = []
-  
-      // method checks if the inputted element exists in the set
-      this.has = (element) => {
-        return collection.indexOf(element) !== -1
+  constructor() {
+    // this array holds the set
+    let collection = []
+
+    // method checks if the inputted element exists in the set
+    this.has = (element) => {
+      return collection.indexOf(element) !== -1
+    }
+
+    // returns and displays all the values in the set
+    this.values = () => {
+      console.log(collection)
+      return collection
+    }
+
+    // method adds an element to the set
+    this.add = (element) => {
+      // performing a check to find out if the element being passed already exists in the set
+      if (!this.has(element)) {
+        collection.push(element)
+        // return true;
+      } // if the element exits returns an error message
+      else {
+        console.error(`${element} already exits`)
+        // return false;
       }
-  
-      // returns and displays all the values in the set
-      this.values = () => {
-        console.log(collection)
-        return collection
+    }
+
+    // this method removes the element (passed into it as an argument) from the set
+    this.remove = (element) => {
+      if (this.has(element)) {
+        index = collection.indexOf(element)
+        collection.splice(index, 1)
       }
-  
+    }
+
+    // returns the size of the set
+    this.size = () => {
+      console.log(collection.length)
+      return collection.length
     }
   }
+}
